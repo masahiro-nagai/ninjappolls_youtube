5dbfb9ad9a06f69cd0b936949c55703f66f818e4
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-29 19:13:29 +0900

    custom form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,8 +1,35 @@
 <script>
     import Button from "../shared/Button.svelte";
     let fields = { question:"",answerA:"",answerB:""};
+    let errors = { question:"",answerA:"",answerB:""};
+    let valid = false;
     const submitHandler = () =>{
-        console.log(fields)
+        valid = true;
+        // varidate question
+        if(fields.question.trim().length < 5){
+            valid = false;
+            errors.question = "質問は５文字以上にしてください"
+        } else{
+            errors.question = "";
+        }
+        // varidate answer A
+        if(fields.answerA.trim().length < 1){
+            valid = false;
+            errors.answerA = "Answer A の空白は無効です"
+        } else{
+            errors.answerA = "";
+        }
+        // varidate answer B
+        if(fields.answerB.trim().length < 1){
+            valid = false;
+            errors.answerB = "Answer B の空白は無効です"
+        } else{
+            errors.answerB = "";
+        }
+        // add new poll
+        if(valid){
+            console.log('valid',fields)
+        }
     }
 </script>
 
@@ -10,14 +37,17 @@
     <div class="form-field">
         <label for="question">Poll Question</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{errors.question}</div>
     </div>
     <div class="form-field">
         <label for="answer-a">Answer A</label>
         <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error">{errors.answerA}</div>
     </div>
     <div class="form-field">
         <label for="answer-b">Answer B</label>
         <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error">{errors.answerB}</div>
     </div>
     <Button type='secondary'flat={true} >Add Poll</Button>
 </form>
@@ -38,5 +68,10 @@
     label{
         margin: 10px auto;
         text-align: left;
+    }
+    .error{
+        color: #d91b42;
+        font-weight: bold;
+        font-size: 12px;
     }
 </style>
