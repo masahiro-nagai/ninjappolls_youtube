c787a60eef1963768a84073974d680b20cfdc460
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-06 00:18:55 +0900

    Lifecycle Hooks

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,15 +1,26 @@
 <script>
+    // import { onMount,onDestroy } from "svelte";
     import PollStore from "../stores/PollStore.js";
     import PollDetails from "./PollDetails.svelte";
-    export let polls = [];
+    
+    // export let polls = [];
 
-    PollStore.subscribe(data => {
-        polls = data;
-    })
+    // const unsub = PollStore.subscribe(data => {
+    //     polls = data;
+    // })
+
+    // onMount(() => {
+    //     console.log("component mounted");
+    // }
+    // )
+    // onDestroy(()=>{
+    //     console.log("component destroy")
+    //     unsub();
+    // })
 </script>
 
 <div class="poll-list">
-    {#each polls as poll(poll.id)}
+    {#each $PollStore as poll(poll.id)}
         <div>
             <PollDetails {poll} on:vote/>
         </div>
