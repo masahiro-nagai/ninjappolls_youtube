553a9a9ca034f4708c45c7f11082335f668da401
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-06 22:33:12 +0900

    Transition Basics

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,27 +1,15 @@
 <script>
-    // import { onMount,onDestroy } from "svelte";
+    import { fade, slide, scale } from "svelte/transition";
+    import {flip} from "svelte/animate";
     import PollStore from "../stores/PollStore.js";
     import PollDetails from "./PollDetails.svelte";
 
-    // export let polls = [];
 
-    // const unsub = PollStore.subscribe(data => {
-    //     polls = data;
-    // })
-
-    // onMount(() => {
-    //     console.log("component mounted");
-    // }
-    // )
-    // onDestroy(()=>{
-    //     console.log("component destroy")
-    //     unsub();
-    // })
 </script>
 
 <div class="poll-list">
     {#each $PollStore as poll(poll.id)}
-        <div>
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
             <PollDetails {poll} />
         </div>
     {/each}
